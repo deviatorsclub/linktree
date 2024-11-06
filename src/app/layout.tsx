088b5f514e5cfe58265f8c06e225372ea4f7caa9
Pulkit <kpulkit15234@gmail.com>
2024-11-06 15:42:21 +0530

    feat: socials open graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,29 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Deviators Club | Get in touch with us",
+  openGraph: {
+    title: "Deviators Club",
+    description: "Get in touch with us",
+    url: "https://linktree.deviatorsdce.tech",
+    siteName: "Deviators Club",
+    images: [
+      {
+        url: "https://linktree.deviatorsdce.tech/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Website preview image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Deviators Club",
+    description: "Get in touch with us",
+    creator: "@devpulkitt",
+    images: ["https://linktree.deviatorsdce.tech/og-image.png"],
+  },
 };
 
 export default function RootLayout({
